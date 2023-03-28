b6fbbd3950b4b94beb4e22d0b94081446bf73e9c
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-28 14:59:59 +1100

    A. userSchema; first pass

--- models/User.js
+++ models/User.js
@@ -1,1 +1,60 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      //TODO: validate uniqueness
+    },
+
+    email: {
+      type: String,
+      required: true,
+      //TODO: validate uniqueness
+      validate: {
+        validator: function (v) {
+          return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid email address!`,
+      },
+      required: [true, "User email address is required"],
+    },
+
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `fullName` that gets and sets the user's full name
+userSchema
+  .virtual("friendCount")
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  });
+
+// Initialize our User model
+const User = model("user", userSchema);
+
+module.exports = User;
