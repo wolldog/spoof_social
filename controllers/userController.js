8f0035af3eae88b766d1a14cd084840073d60c71
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-30 18:00:09 +1100

    M. userController; add getUsers & getUser by ID

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,29 @@
 const { User, Thought} = require('../models');
 
+//GET all users
+module.exports = {
+    getUsers(req, res) {
+        User.find()
+        .then((users) => res.json(users))
+        .catch((err) => res.status(500).json(err))
+    },
+//GET a single user by _id
+        getUser(req, res) {
+            User.findOne(({ _id: req.params.id}))
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err))
+        },
+};
 
-module.exports = {};
+
+
+
+//POST a new user
+
+//PUT to update a user by _id
+
+//DELETE a user by _id
+
+//BONUS: Remove a user's associated thoughts when deleted
+
+
