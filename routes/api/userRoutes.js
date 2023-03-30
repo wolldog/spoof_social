056fefa5358038a05c1282b211ae46db680f81ce
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-30 17:59:32 +1100

    M. userRoutes; add getUsers & getUser

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
 
+const {
+    getUsers,
+    getUser
+} = require('../../controllers/userController')
+
+//Get all users
+router.route('/').get(getUsers)
+
+//Get user by ID
+router.route('/:id').get(getUser)
+
 
 module.exports = router;
