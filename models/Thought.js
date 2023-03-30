066cf75fc0bdb7806ca3944ee286638d692ad7b5
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-30 13:09:43 +1100

    A. Thoughts model; first pass

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,50 @@
 const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
+
+const thoughtSchema = new Schema(
+
+    {
+        thoughtText: {
+            type: String,
+            require: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+
+        createdAt: {
+
+            type: Date,
+            default: Date.now,
+            //get: v => format date
+        },
+
+        username: {
+            type: String,
+            require: true,
+
+        },
+
+        reactions: [Reaction]
+
+    },
+    {
+        // Include virtuals with our response, overriding the default behavior
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+      }
+    
+);
+
+thoughtSchema
+  .virtual("reactionCount")
+  // Getter
+  .get(function () {
+    return this.reactions.length;
+  });
+
+// Initialize our User model
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought
