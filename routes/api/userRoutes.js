04b8a9a9c9302ee9e6bf1be3469a6f984d2d3899
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-31 15:04:23 +1100

    M. userRoutes.js; add/update/deleteUser

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,14 +2,24 @@ const router = require('express').Router();
 
 const {
     getUsers,
-    getUser
+    getUser,
+    addUser,
+    updateUser,
+    deleteUser,
 } = require('../../controllers/userController')
 
-//Get all users
-router.route('/').get(getUsers)
+//GET all users & POST a new user
+router.route('/').get(getUsers).post(addUser)
 
-//Get user by ID
-router.route('/:id').get(getUser)
+//GET user by ID, Update a user by ID & DELETE a user by ID
+router.route('/:id').get(getUser).put(updateUser).delete(deleteUser)
+
+
+//PUT to update a user by _id
+
+
+//DELETE a user by _id
+
 
 
 module.exports = router;
