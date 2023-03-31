b8f0eff4dfa2f35735deeb3e19a613bb94b6738c
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-31 15:51:56 +1100

    M. thoughtController.js: add getThts & getThtsbyId

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,5 +1,18 @@
-const { Thought, User } = require('../models');
+const { Thought, User, Reaction } = require('../models');
+
+//GET all thoughts
+module.exports = {
+    getThoughts(req, res) {
+      Thought.find()
+        .then((thoughts) => res.json(thoughts))
+        .catch((err) => res.status(500).json(err));
+    },
+    getThought(req, res) {
+        Thought.findOne({ _id : req.params.thoughtId})
+        .then((thought) => res.json(thought))
+        .catch((err) => res.status(500).json(err));
+    },
+    }
 
 
 
-module.exports = {};
