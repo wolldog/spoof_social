bc43af140acdc930bcc8bafc5ec47fcaa94daa9f
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-31 15:52:45 +1100

    M. thoughtRoutes.js; add getThoughts & getThought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
 
+const {
+    getThoughts,
+    getThought,
+
+} = require('../../controllers/thoughtController')
+
+//GET all thoughts & POST a new thought
+router.route('/').get(getThoughts)
+
+//GET thought by ID
+router.route('/:thoughtId').get(getThought)
 
 module.exports = router;
