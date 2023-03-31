00aca38874219929d1e75a3a06080c8b6ddd0aad
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-31 15:03:38 +1100

    M. userController.js: add/update/deleteUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,55 @@
-const { User, Thought} = require('../models');
+const { User, Thought } = require("../models");
 
 //GET all users
 module.exports = {
-    getUsers(req, res) {
-        User.find()
-        .then((users) => res.json(users))
-        .catch((err) => res.status(500).json(err))
-    },
-//GET a single user by _id
-        getUser(req, res) {
-            User.findOne(({ _id: req.params.id}))
-            .then((users) => res.json(users))
-            .catch((err) => res.status(500).json(err))
-        },
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  //GET a single user by _id
+  getUser(req, res) {
+    User.findOne({ _id: req.params.id })
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  //POST a new user
+  addUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  //PUT to update a user by _id
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with this id!" })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  //DELETE a user by _id
+  deleteUser(req, res) {
+    User.findByIdAndDelete({ _id: req.params.id })
+        .then((user) =>
+        !user
+        ? res.status(404).json({ message: "No user with this id!" })
+//TODO: include delete of users associated thoughts
+        : res.json({ message: "One user deleted" })
+        )
+        .catch((err) => res.status(500).json(err));
+  }
+
 };
 
 
 
-
-//POST a new user
-
-//PUT to update a user by _id
-
-//DELETE a user by _id
-
 //BONUS: Remove a user's associated thoughts when deleted
-
-
