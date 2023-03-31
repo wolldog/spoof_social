8808c78e9e57cf849db1443ac36f34101066762c
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-03-31 19:00:25 +1100

    M. thoughtRoutes; add/update/deleteThought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,13 +3,19 @@ const router = require('express').Router();
 const {
     getThoughts,
     getThought,
+    addThought,
+    updateThought,
+    deleteThought,
+   
 
 } = require('../../controllers/thoughtController')
 
 //GET all thoughts & POST a new thought
-router.route('/').get(getThoughts)
+router.route('/').get(getThoughts).post(addThought)
 
-//GET thought by ID
-router.route('/:thoughtId').get(getThought)
+//GET thought by ID & PUT thought by ID & DELETE thought by ID
+router.route('/:thoughtId').get(getThought).put(updateThought).delete(deleteThought)
+
+//POST reaction to thought by ID & DELETE reaction
 
 module.exports = router;
