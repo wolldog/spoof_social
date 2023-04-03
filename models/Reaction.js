f0bf64fc70bdeb00630c2793f5c356b4e672cea2
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-03 14:40:22 +1000

    M. Reaction.js; add getter method to format date

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,27 +1,38 @@
 const { Schema, Types } = require("mongoose");
 
-const reactionSchema = new Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
-  },
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
 
-  reactionBody: {
-    type: String,
-    require: true,
-    maxLength: 280,
-  },
+    reactionBody: {
+      type: String,
+      require: true,
+      maxLength: 280,
+    },
 
-  username: {
-    type: String,
-    require: true,
-  },
+    username: {
+      type: String,
+      require: true,
+    },
 
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    //get: v => format date, moment?
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAt) => {
+        return createdAt.toLocaleDateString();
+      },
+    },
   },
-});
+  {
+    // Include virtuals with the response, overriding the default behavior
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 module.exports = reactionSchema;
