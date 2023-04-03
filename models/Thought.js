081b3d17b688496ff5713cd24ee9922d2f99b717
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-03 13:33:37 +1000

    M. Thoughts.js; modify reactions reference

--- models/Thought.js
+++ models/Thought.js
@@ -1,40 +1,35 @@
-const { Schema, model } = require('mongoose');
-const Reaction = require('./Reaction');
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
 
 const thoughtSchema = new Schema(
-
-    {
-        thoughtText: {
-            type: String,
-            require: true,
-            minLength: 1,
-            maxLength: 280,
-        },
-
-        createdAt: {
-
-            type: Date,
-            default: Date.now,
-            //get: v => format date
-        },
-
-        username: {
-            type: String,
-            require: true,
-
-        },
-
-        reactions: [Reaction]
-
+  {
+    thoughtText: {
+      type: String,
+      require: true,
+      minLength: 1,
+      maxLength: 280,
     },
-    {
-        // Include virtuals with our response, overriding the default behavior
-        toJSON: {
-          virtuals: true,
-        },
-        id: false,
-      }
-    
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //get: v => format date
+    },
+
+    username: {
+      type: String,
+      require: true,
+    },
+
+    reactions: [reactionSchema],
+  },
+  {
+    // Include virtuals with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
 );
 
 thoughtSchema
@@ -47,4 +42,4 @@ thoughtSchema
 // Initialize our User model
 const Thought = model("thought", thoughtSchema);
 
-module.exports = Thought
+module.exports = Thought;
