9794150ec3490f019bcca80bad989cd7ba0a75b6
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-03 13:32:39 +1000

    M. thoughtRoutes; add POST/DELETE reaction routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -6,6 +6,8 @@ const {
     addThought,
     updateThought,
     deleteThought,
+    addReaction,
+    deleteReaction
    
 
 } = require('../../controllers/thoughtController')
@@ -16,6 +18,10 @@ router.route('/').get(getThoughts).post(addThought)
 //GET thought by ID & PUT thought by ID & DELETE thought by ID
 router.route('/:thoughtId').get(getThought).put(updateThought).delete(deleteThought)
 
-//POST reaction to thought by ID & DELETE reaction
+//POST reaction to thought by ID
+router.route('/:thoughtId/reactions').post(addReaction)
+
+//DELETE reaction to thought by ID
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction)
 
 module.exports = router;
