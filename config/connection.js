07b39e7cee3ff0568d6a62f007c02b0c533fc09c
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-04 16:47:46 +1000

    M. connection.js; comment, formatting

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 const { connect, connection } = require("mongoose");
 
+// Wrap Mongoose around local connection to MongoDB
 connect("mongodb://127.0.0.1:27017/spoofSocial", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// Export connection
 module.exports = connection;
