85a47ce043f9e4c2adfb6ea27c46660d497cbd38
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-04 16:12:37 +1000

    M. User.js; ref to model to lowercase

--- models/User.js
+++ models/User.js
@@ -26,14 +26,14 @@ const userSchema = new Schema(
     thoughts: [
       {
         type: Schema.Types.ObjectId,
-        ref: "Thought",
+        ref: "thought",
       },
     ],
 
     friends: [
       {
         type: Schema.Types.ObjectId,
-        ref: "User",
+        ref: "user",
       },
     ],
   },
