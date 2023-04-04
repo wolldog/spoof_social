dc24e30f3e73aab66d5ed26565026134ee017ca9
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-04 17:34:52 +1000

    M. index.js; comment, formatting

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const db = require('./config/connection');
-const { User, Thought, Reaction } = require('./models')
-const routes = require('./routes');
+// Import express package
+const express = require("express");
+// Declare variable for connection to database
+const db = require("./config/connection");
+// Provide path for API routes
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
+// Initialize app variable by setting it to the value of express()
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+// Built in Express function that parses incoming requests to JSON
 app.use(express.json());
 app.use(routes);
 
-db.once('open', () => {
+//Open connection to the database
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server for Spoof Social is running on port ${PORT}!`);
   });
