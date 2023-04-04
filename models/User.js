c137062da96a8c0305a0ddc5492f4ea887f09c92
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-04 17:05:10 +1000

    M. User.js; comment, formatting

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+// Define Mongoose
 const { Schema, model } = require("mongoose");
 
 // Schema to create User model
@@ -5,31 +6,34 @@ const userSchema = new Schema(
   {
     username: {
       type: String,
-      required: [ true, "A username is required" ],
-      trim: true,
-      unique: true
+      required: [true, "A username is required"], //username is required
+      trim: true, //trim whitespace
+      unique: true, //require username to be unique
     },
 
     email: {
       type: String,
-      required: true,
+      required: true, //username is required
       validate: {
+        //regex to validate the email address
         validator: function (v) {
           return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
         },
-        message: (userEmail) => `${userEmail.value} is not a valid email address!`,
+        message: (userEmail) =>
+          `${userEmail.value} is not a valid email address!`,
       },
-      required: [true, "User email address is required"],
-      unique: true
+      required: [true, "User email address is required"], //email is required
+      unique: true, //require email to be unique
     },
 
+    //path to include the users thoughts
     thoughts: [
       {
         type: Schema.Types.ObjectId,
         ref: "thought",
       },
     ],
-
+    //path to include the users friends
     friends: [
       {
         type: Schema.Types.ObjectId,
